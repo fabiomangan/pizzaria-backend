e15190b79388918c49e8a41592b0052c8c500520
fabiomangan <fabio.mangan99@hotmail.com>
2025-12-12 11:46:18 -0300

    Criando validação de schema e configurando schema do projeto

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
-import { Router, Request, Response } from 'express'
+import { Router } from 'express'
 import { CreateUserController } from './controllers/user/CreateUserController'
+import { validateSchema } from './middlewares/validateSchema';
+import { createUserSchema } from './schemas/userSchema';
 
 const router = Router();
 
-router.post("/users", new CreateUserController().handle);
+router.post("/users", validateSchema(createUserSchema), new CreateUserController().handle);
 
 export { router };
